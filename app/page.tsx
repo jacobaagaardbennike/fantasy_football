7c369c499b437fd6d8d3f62b324b35d50678e6af
Jacob Ågård Bennike <jab@obton.com>
2023-02-01 12:54:36 +0100

    hoho

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,37 @@
-import { FC } from 'react'
+'use client';
 
-interface pageProps {
-  
-}
+import { useDraw } from '@/hooks/useDraw';
+import { FC } from 'react';
+
+interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>page</div>
-}
+  const { canvasRef, onMouseDown } = useDraw(drawLine);
 
-export default page
+  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
+    const { x: currX, y: currY } = currentPoint;
+    const lineColor = '#000';
+    const lineWidth = 5;
+
+    let startPoint = prevPoint ?? currentPoint;
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = lineColor;
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+
+    ctx.fillStyle = lineColor;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  return (
+    <div className="w-screen h-screen bg-white flex justify-center items-center">
+      <canvas ref={canvasRef} onMouseDown={onMouseDown} width={750} height={750} className="border border-black rounded-md" />
+    </div>
+  );
+};
+
+export default page;
