9884995049a1f25fb0e52b45a183c87b406731d5
Jacob Ågård Bennike <jab@obton.com>
2023-02-03 22:53:58 +0100

    7s board

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { InitOverworld } from '@/hooks/overworld';
+// import { InitOverworld } from '@/hooks/overworld';
+import { UseDrawBoard } from '@/hooks/sevensBoard';
 import { FC } from 'react';
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  const { canvasRef } = InitOverworld();
+  const { canvasRef } = UseDrawBoard();
 
   return (
     <div className="w-screen h-screen bg-white flex justify-center items-center">
-      <canvas ref={canvasRef} width={750} height={750} className="border border-black rounded-md" />
+      <canvas ref={canvasRef} className="border border-black rounded-md" />
     </div>
   );
 };
---
+++ hooks/sevensBoardv1.ts
@@ -0,0 +1,61 @@
+import { useRef, useEffect } from 'react';
+
+interface Square {
+  color: string;
+}
+
+interface SquareMatrix {
+  squares: Square[][];
+}
+
+export const UseDrawBoard = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      if (!ctx) return;
+
+      const width = 1200;
+      const height = (width / 20) * 11;
+      canvas.width = width;
+      canvas.height = height;
+
+      const squareWidth = width / 20;
+      const squareHeight = height / 11;
+
+      const matrix: SquareMatrix = {
+        squares: Array.from({ length: 20 }, (_, h) =>
+          Array.from({ length: 11 }, (_, v) => {
+            return { color: h === 0 ? 'blue' : h === 19 ? 'red' : 'green' };
+          }),
+        ),
+      };
+
+      matrix.squares.forEach((row, h) => {
+        row.forEach((square, v) => {
+          ctx.fillStyle = square.color;
+          ctx.fillRect(h * squareWidth, v * squareHeight, squareWidth, squareHeight);
+        });
+      });
+
+      ctx.strokeStyle = 'white';
+      ctx.lineWidth = 1;
+      for (let i = 0; i <= 20; i++) {
+        ctx.beginPath();
+        ctx.moveTo(i * squareWidth, 0);
+        ctx.lineTo(i * squareWidth, height);
+        ctx.stroke();
+      }
+      for (let i = 0; i <= 11; i++) {
+        ctx.beginPath();
+        ctx.moveTo(0, i * squareHeight);
+        ctx.lineTo(width, i * squareHeight);
+        ctx.stroke();
+      }
+    }
+  }, [canvasRef]);
+
+  return { canvasRef };
+};
